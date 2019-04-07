d0c836ad1d3b194bb7dd2664677cf3c94e45b812
Carlos Orellana <carlos.cw2106@gmail.com>
2019-04-07 17:53:18 -0600

    Created seeds in order to load data for Apartments model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Apartment.destroy_all
+
+CSV.foreach("Sacramentorealestatetransactions.csv", headers: true) do |line|
+  Apartment.create! line.to_hash.except(*%w{type latitude longitude})
+end
