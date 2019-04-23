1ebeacabc62c68880dd5721c4a2c33bba13ae99c
Carlos Orellana <carlos.cw2106@gmail.com>
2019-04-22 22:15:48 -0600

    Added the functionality when you make zoom and move the map, the bounding box of the search is reduced to the closest places. This reduces even the results of the pagination, since is filtering from the center and the sw_lat and sw_lng calculating the distance between the center of those mentioned points of reference

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -6,14 +6,16 @@ class ApartmentsController < ApplicationController
   def index
     @apartments = if params[:l]
                   sw_lat, sw_lng, ne_lat, ne_lng = params[:l].split(",")
-                  center = Geocoder::Calculations.geographic_center([[sw_lat, sw_lng], [ne_lat, ne_lng]])
-                  Apartment.near(center)
+                  center   = Geocoder::Calculations.geographic_center([[sw_lat, sw_lng], [ne_lat, ne_lng]])
+                  distance = Geocoder::Calculations.distance_between(center, [sw_lat, sw_lng])
+                  box      = Geocoder::Calculations.bounding_box(center, distance)
+                  Apartment.within_bounding_box(box)
                 elsif params[:near]
                     Apartment.near(params[:near])
                   else
                     Apartment.all
                   end
-    @apartments = @apartments.page(params[:page]).per(15)
+    @apartments = @apartments.page(params[:page]).per(5)
   end
 
   # GET /apartments/1
