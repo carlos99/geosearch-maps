b9c0e3a5df70fffebb6b6ad2965dcf650e617a87
Carlos Orellana <carlos.cw2106@gmail.com>
2019-05-07 15:43:43 -0600

    Added bootstrap via webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,11 @@
 // const imagePath = (name) => images(name, true)
 
 require('./maps')
+
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
